cbdef07490171012119140b544679bdd78d29c28
bg-shorthand <hanhanhan333@gmail.com>
2022-04-20 14:12:32 +0900

    style: dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,49 @@
-import { Grid, Typography } from "@mui/material";
+import {
+  createTheme,
+  CssBaseline,
+  Grid,
+  Paper,
+  ThemeProvider,
+  Typography,
+} from "@mui/material";
 import styled from "@emotion/styled";
 import AddTodos from "./observers/AddTodos";
 import ActiveList from "./observers/ActiveLIst";
 import CompletedList from "./observers/CompletedList";
 
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
 function App() {
   return (
-    <AlignCenter>
-      <GridContainer container spacing={2}>
-        <Grid item xs={12}>
-          <Typography variant="h3" component="h1">
-            Todos with MobX
-          </Typography>
-        </Grid>
-        <Grid item xs={12}>
-          <AddTodos />
-        </Grid>
-        <Grid item xs={6}>
-          <ActiveList />
-        </Grid>
-        <Grid item xs={6}>
-          <CompletedList />
-        </Grid>
-      </GridContainer>
-    </AlignCenter>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <AlignCenter>
+        <GridContainer container spacing={2}>
+          <Grid item xs={12}>
+            <Typography variant="h3" component="h1">
+              Todos with MobX
+            </Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <AddTodos />
+          </Grid>
+          <Grid item xs={6}>
+            <ActiveList />
+          </Grid>
+          <Grid item xs={6}>
+            <CompletedList />
+          </Grid>
+        </GridContainer>
+      </AlignCenter>
+    </ThemeProvider>
   );
 }
 
-const AlignCenter = styled.div`
+const AlignCenter = styled(Paper)`
   width: 100vw;
   height: 100vh;
   display: flex;
